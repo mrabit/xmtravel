77fdfa9c2d8792e2085b6f204460d98b385b8d80
mrabit <yuany@mrabit.com>
2022-06-30 16:43:35 +0800

    chore: update schedule time

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const schedule = require('node-schedule')
 const api = require('./api')
 
 api.init()
-schedule.scheduleJob('00 */20 * * * *', api.init) // 每20分钟执行一次
+schedule.scheduleJob('00 */20 9-20 * * *', api.init) // 每20分钟执行一次
